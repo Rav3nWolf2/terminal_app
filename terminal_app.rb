8f5d9165891492f47e0ba4f8742ff69f6e218195
beth94victoria <beth.callanan@gmail.com>
2018-09-04 15:09:43 +1000

    added testing files

--- terminal_app.rb
+++ terminal_app.rb
@@ -1,6 +1,39 @@
+require 'test/unit'
+
+class Question
+attr_accessor :prompt, :answer
+    def initialize (prompt, answer)
+        @prompt = prompt
+        @answer = answer
+    end
+end
 puts "welcome to the ____________ Quiz"
 puts "test your popculture knowledge"
 puts "are you ready?"
-puts "let's go!" 
+puts "< PRESS ENTER TO START >!" 
 userinput= gets.chomp# first greeting 
+
+
+# how about we have different arrays like (M1-5 = Movie) questions (G1-5= gaming) (T1-5= television) (Mu1-5= Music)
+M1 = ["Luke Skywalker is from what franchise?\n(a)StarTrek\n(b)StarWars\n(c)TrekWars"]
+M2 = [""]
+M3 = [""]
+M4 = [""]
+M5 = [""]
+T1 = [""]
+T2 = [""]
+T3 = [""]
+T4 = [""]
+T5 = [""]
+
+# should we make a class of arrays? or store arrays in a hash
+
+
+
+
+
+
+
+# define before using methods
+
 
