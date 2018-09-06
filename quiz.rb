95567cef6773f2729492ddae253b2d5b78727f77
beth94victoria <beth.callanan@gmail.com>
2018-09-06 11:25:46 +1000

    failed attempt to print the score

--- quiz.rb
+++ quiz.rb
@@ -17,14 +17,14 @@ class Quiz
     end
     def set_default_questions
         @questions << Question.new("Luke Skywalker is from what franchise?", {a: "Star trek", b: "Star wars", c: "Trek wars"}, :b )
-        @questions << Question.new("Who said?, you're a wizard Harry?", {a: "Dolores Umbride", b: "harry Potter", c: "Hagrid"}, :c )
+        @questions << Question.new("Who said?, you're a wizard Harry?", {a: "Dolores Umbride", b: "harry Potter", c: "Rubeus Hagrid"}, :c )
         @questions << Question.new("Judy Garland starred in this 1939 movie", {a: "The wizard of OZ", b: "The Meg", c: "Annie"}, :a )
-        @questions << Question.new("What musical had a live!  version in 2016 but originally came out in 1988?", {a: "grease", b: "hairspray", c: "High school musical"}, :b )
+        @questions << Question.new("What musical had a live! tv version in 2016 but originally came out in 1978?", {a: "grease", b: "hairspray", c: "High school musical"}, :a )
         @questions << Question.new("What was the first movie to earn the title 'BLOCKBUSTER?'--HINT 1975", {a:"the rocky horror picture show", b:"Jaws", c:"sharknado"}, :b )
-        @questions << Question.new("what Tv show started in 1994 and ended in 2004?", {a: "Mash", b: "Home and Away", c: "Friends"}, :c )
+        @questions << Question.new("what Tv show started in 1994 and ended in 2004?", {a: "Friends", b: "Home and Away", c: "Mash"}, :a )
         @questions << Question.new("Which tv family lives in Springfield?", {a: "Family Guy", b: "The Simpsons", c: "The Kardashians"}, :b )
         @questions << Question.new("Which Australian tv family lives in a light house?", {a: "The Lights's", b:"The House's", c: "The Twist's"}, :c )
-        @questions << Question.new("What tv show ended in 1998 after 9 seasons?", {a: "The Bachelor", b: "Seinfeld", c: "Cheers"}, :b )
+        @questions << Question.new("What tv show ended in 1998 after 9 seasons?", {a: "Seinfeld", b: "The Bachelor", c: "Cheers"}, :a )
         @questions << Question.new("who played the 10th doctor in Doctor who?", {a: "Matt Smith", b: "Nigel Bottom tooth the3rd", c: "David tenant"}, :c )
     end
 end
--- terminal_app.rb
+++ terminal_app.rb
@@ -2,28 +2,29 @@ require_relative 'quiz'
 
 class TerminalApp
     def initialize 
-        @quiz = Quiz.new  
+        @quiz = Quiz.new 
+        @total_score = 0 
     end  
-    def ask_question(question_info)
+    def ask_question(question_info) # asks the user for the question answers get checked
         puts question_info.question
         puts question_info.answer_choices
         userinput = gets.chomp.to_sym
         check_answer(userinput,question_info.correct_answer)
     end
 
-    def check_answer(userinput,correct_answer)
+    def check_answer(userinput,correct_answer) # implements score if correct answer is imputed 
         @quiz.total_score += 1 if userinput == correct_answer
     end
     def run
-        puts "welcome to the  Quiz"
+        puts "welcome to the FT quiz"
         puts "test your popculture knowledge"
         puts "are you ready?"
         puts "< PRESS ENTER TO START >!" 
         userinput = gets.chomp 
-        random_questions = @quiz.questions.sample(@quiz.questions.length) # expand the questions in quiz without having to change anything here
-        puts "quiz.questions: #{@quiz.questions.length}"
+        random_questions = @quiz.questions.sample(@quiz.questions.length) # add or delete questions in quiz without having to change anything here
+        puts "quiz.questions: #{@quiz.questions.length}"#  get a question at random 
         # random_questions.each do |question| 
-        for question in random_questions do
+        for question in random_questions do # 
             # rng = Random.new
             # index = rng.rand(0..@quiz.questions.length)
             # question_info = questions[index]
@@ -44,8 +45,9 @@ class TerminalApp
 end
 terminal_app = TerminalApp.new
 terminal_app.run
+puts "your score is #{@total_score}"
 terminal_app.print_results
-@quiz.total_score
+
 
 
 
